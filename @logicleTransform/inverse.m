976d471bcee70c2cb20f556f1625552c9fc36a30
Harley Day <harley.day4@gmail.com>
2018-07-17 19:35:49 +0100

    Reinstate the multidimentional object management

--- @logicleTransform/inverse.m
+++ @logicleTransform/inverse.m
@@ -2,31 +2,31 @@ function out = inverse(obj,data_in)
 % Paraphrasing of c++ implementation by Wayne A. Moore found at
 % http://onlinelibrary.wiley.com/doi/10.1002/cyto.a.22030/full
 %% preable to determine how to treat multidimensional input
-% szo = size(obj);
-% szd = size(data_in);
-% if szo==[1,1] % if scalar object, apply this to the whole data_in array
-%     obji = @(i) 1;
-% elseif szo==szd % if size of object is the same as that of data_in, apply each object to it's corresponding element in the array
-%     obji = @(i) i;
-% elseif szo(2)==szd(2)&&szo(1)==1
-%     obji = @(i) mod(floor((i-1)/szd(1)),szd(2))+1;
-% else
-%     error('Size of LogicleTransform object must be scalar, vector or same size as data_in');
-% end
+szo = size(obj);
+szd = size(data_in);
+if szo==[1,1] % if scalar object, apply this to the whole data_in array
+    obji = @(i) 1;
+elseif szo==szd % if size of object is the same as that of data_in, apply each object to it's corresponding element in the array
+    obji = @(i) i;
+elseif szo(2)==szd(2)&&szo(1)==1
+    obji = @(i) mod(floor((i-1)/szd(1)),szd(2))+1;
+else
+    error('Size of LogicleTransform object must be scalar, vector or same size as data_in');
+end
 %%
 out = zeros(size(data_in));
 for i = 1:length(data_in(:))
-    negative = data_in(i) < obj.x1;
+    negative = data_in(i) < obj(obji(i)).x1;
     if (negative)
-        data_in(i) = 2*obj.x1 - data_in(i);
+        data_in(i) = 2*obj(obji(i)).x1 - data_in(i);
     end
     % compute the biexponential
-    if (data_in(i) < obj.xTaylor)
+    if (data_in(i) < obj(obji(i)).xTaylor)
         % near x1, i.e., data zero use the series expansion
-        inverse = obj.seriesBiexponential(data_in(i));
+        inverse = obj(obji(i)).seriesBiexponential(data_in(i));
     else
         % this formulation has better roundoff behavior
-        inverse = (obj.a*exp(obj.b*data_in(i)) + obj.f) - obj.c/exp(obj.d*data_in(i));
+        inverse = (obj(obji(i)).a*exp(obj(obji(i)).b*data_in(i)) + obj(obji(i)).f) - obj(obji(i)).c/exp(obj(obji(i)).d*data_in(i));
     end
     
     % handle scale(i) for negative values
--- @logicleTransform/transform.m
+++ @logicleTransform/transform.m
@@ -2,29 +2,29 @@ function data_out = transform(obj,data_in)
 % Paraphrasing of c++ implementation by Wayne A. Moore found at
 % http://onlinelibrary.wiley.com/doi/10.1002/cyto.a.22030/full
 %% preable to determine how to treat multidimensional input
-% szo = size(obj);
-% szd = size(data_in);
-% if szo==[1,1] % if scalar object, apply this to the whole data_in array
-%     obji = @(i) 1;
-% elseif szo==szd % if size of object is the same as that of data_in, apply each object to it's corresponding element in the array
-%     obji = @(i) i;
-% elseif szo(2)==szd(2)&&szo(1)==1
-%     obji = @(i) mod(floor((i-1)/szd(1)),szd(2))+1;
-% else
-%     error('Size of LogicleTransform object must be scalar, vector or same size as data_in');
-% end
+szo = size(obj);
+szd = size(data_in);
+if szo==[1,1] % if scalar object, apply this to the whole data_in array
+    obji = @(i) 1;
+elseif szo==szd % if size of object is the same as that of data_in, apply each object to it's corresponding element in the array
+    obji = @(i) i;
+elseif szo(2)==szd(2)&&szo(1)==1
+    obji = @(i) mod(floor((i-1)/szd(1)),szd(2))+1;
+else
+    error('Size of LogicleTransform object must be scalar, vector or same size as data_in');
+end
 %%
 data_out = zeros(size(data_in));
 for i = 1:length(data_in(:))
-    if ~isempty(obj.n_bins)
+    if ~isempty(obj(obji(i)).n_bins)
         % lookup the bin into which this data point falls and return the left edge of the bin
-        index = BinSearch(obj.lookup,data_in(i));
+        index = BinSearch(obj(obji(i)).lookup,data_in(i));
         
         % inverse interpolate the table linearly
-        delta = (data_in(i)-obj.lookup(index))./(obj.lookup(index+1)-obj.lookup(index));
-        data_out(i) = (index-1+delta)/obj.n_bins;
+        delta = (data_in(i)-obj(obji(i)).lookup(index))./(obj(obji(i)).lookup(index+1)-obj(obji(i)).lookup(index));
+        data_out(i) = (index-1+delta)/obj(obji(i)).n_bins;
     else
-        data_out(i) = numerically_invert(obj,data_in(i));
+        data_out(i) = numerically_invert(obj(obji(i)),data_in(i));
     end
 end
 
