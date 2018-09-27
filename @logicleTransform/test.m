992b20a019e87e9566702db32fc1e41415079c01
Harley Day <harley.day13@imperial.ac.uk>
2018-09-27 15:14:15 +0100

    Update link and don't use try catch

--- @logicleTransform/test.m
+++ @logicleTransform/test.m
@@ -2,20 +2,18 @@ function results = test()
 % static method to show unit testing results to the user.
 % >> results = logicleTransform.test;
 % this can be called without instantiating a logicleTransform object.
-disp('Checking logicleTransform.m toolbox performance using the packaged test suite.');
+disp ( 'Checking logicleTransform.m toolbox performance using the packaged test suite.' );
 %% run the unit test suite
 import matlab.unittest.TestSuite;
 
-try
-    suite = TestSuite.fromClass ( ?logicleTransformTest );
-    results = run ( suite );
-    display ( results );
+suite = TestSuite.fromClass ( ?logicleTransformTest );
+results = run ( suite );
+display ( table(results) );
+if any ( [results.Failed] )
+    disp( 'Logicle Transform toolbox contains errors. Please register this issue at the <a href="https://github.com/harleyday/logicleTransform.m/issues/new/choose">GitHub repository issues page</a>.' );
+    disp( 'Thank you for your time, and sorry for the inconvenience.' );
+else
     disp( 'Tests passed with no errors. Enjoy!' );
     disp( 'User manual can be found at the <a href="https://harleyday.github.io/logicleTransform.m/">GitHub Pages site</a>.' );
-catch e
-    display ( results );
-    display( getReport ( e, 'extended' ) );
-    disp( 'Logicle Transform toolbox contains errors. Please register an issue at the <a href="https://github.com/harleyday/logicleTransform.m">GitHub repository</a>.' );
-    disp( 'Thank you for your time, and sorry for the inconvenience.' );
 end
 end
